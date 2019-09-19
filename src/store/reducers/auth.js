9cd0a3a65bce184d3adbb4553902d82e1ef8fccc
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-09-19 13:25:02 +0200

    feat: boiler template

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,2 +1,17 @@
-function auth() {}
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+	loading: false
+};
+
+function auth(state = initialState, action) {
+	switch (action.type) {
+		case actionTypes.AUTH_START:
+			return { ...state, loading: true };
+
+		default:
+			return state;
+	}
+}
+
 export default auth;
